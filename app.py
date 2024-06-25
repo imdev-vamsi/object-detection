bd57eac7b74e6eb22637c69ed840bf8c9d4f9fce
Ayyappa <21B91A6132@srkrec.ac.in>
2024-06-26 01:43:11 +0530

    search updated

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template, Response,redirect,url_for
 import os
 from process import generate_frames
+import time
 
 app = Flask(__name__)
 
@@ -20,7 +21,8 @@ def upload_file():
     if file:
         input_path = r"static/uploads/input.mp4"
         file.save(input_path)
-        return render_template('index.html',send_file=input_path)  
+        timestamp = int(time.time())
+        return render_template('index.html',send_file=input_path, timestamp=timestamp)  
 
     
 @app.route('/video_feed')
@@ -33,7 +35,8 @@ def video_feed():
 @app.route('/search', methods=['POST'])
 def search():
     object_name = request.form['name']
-    return redirect(url_for('video_feed', object_name=object_name))
+    timestamp = int(time.time())
+    return render_template('index.html', object_name=object_name, timestamp=timestamp)
 
 if __name__ == "__main__":
     os.makedirs('uploads', exist_ok=True)
--- process.py
+++ process.py
@@ -3,10 +3,10 @@ import torch
 model = torch.hub.load("ultralytics/yolov5", "yolov5s")
 
 import cv2
-from flask import Response
 
 def generate_frames(input_video_path, target_label):
     cap = cv2.VideoCapture(input_video_path)
+    # total_object_count = 0  # Initialize total object count
 
     while cap.isOpened():
         ret, frame = cap.read()
@@ -16,22 +16,28 @@ def generate_frames(input_video_path, target_label):
         # Convert frame to RGB
         img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         results = model(img)
-        if target_label:
-            # Draw bounding boxes
-            for result in results.xyxy[0]:
-                x1, y1, x2, y2, conf, cls = result
-                label = f"{model.names[int(cls)]} {conf:.2f}"
-                if label[:-5] == target_label:
-                    cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (255, 255, 255), 2)
-                    cv2.putText(frame, label, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (91, 218, 227), 2)
-        else:
-            # Draw bounding boxes
-            for result in results.xyxy[0]:
-                x1, y1, x2, y2, conf, cls = result
-                label = f"{model.names[int(cls)]} {conf:.2f}"
 
+        if target_label:
+            # Draw bounding boxes for the specified target label
+            frame_object_count = 0  # Count objects in the current frame
+            for result in results.xyxy[0]:
+                x1, y1, x2, y2, conf, cls = result
+                label = model.names[int(cls)]
+                if label == target_label:
+                    frame_object_count += 1
+                    # total_object_count += 1
+                    cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (255, 255, 255), 2)
+                    cv2.putText(frame, f"{label} {conf:.2f}", (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (91, 218, 227), 2)
+            # Display count for the current frame and total count
+            cv2.putText(frame, f'{target_label} Count: {frame_object_count}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (91, 218, 227), 2)
+            # cv2.putText(frame, f'Total Count: {total_object_count}', (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+        else:
+            # Draw bounding boxes for all detected objects if no target label is specified
+            for result in results.xyxy[0]:
+                x1, y1, x2, y2, conf, cls = result
+                label = model.names[int(cls)]
                 cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (255, 255, 255), 2)
-                cv2.putText(frame, label, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (91, 218, 227), 2)
+                cv2.putText(frame, f"{label} {conf:.2f}", (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (91, 218, 227), 2)
 
         ret, buffer = cv2.imencode('.jpg', frame)
         frame = buffer.tobytes()
@@ -39,4 +45,5 @@ def generate_frames(input_video_path, target_label):
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
     cap.release()
+
     cv2.destroyAllWindows()
